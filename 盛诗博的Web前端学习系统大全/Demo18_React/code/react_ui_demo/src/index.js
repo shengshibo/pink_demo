449f93aeff6b11f8c8794999b71abf886981748c
shengshibo <shengshibo@tom.com>
2019-09-05 11:47:55 +0800

    前端学习和hack笔记

---
+++ 盛诗博的Web前端学习系统大全/Demo18_React/code/react_ui_demo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from 'react-dom'
+import 'antd-mobile/dist/antd-mobile.css'
+
+import App from './components/app'
+
+render (<App />, document.getElementById('root'))
