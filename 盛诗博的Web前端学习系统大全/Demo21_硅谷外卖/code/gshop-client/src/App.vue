084b032072689ece00dfd4681afc481250206a3d
shengshibo <shengshibo@tom.com>
2019-09-24 14:28:35 +0800

    前端学习

--- 盛诗博的Web前端学习系统大全/Demo21_硅谷外卖/code/gshop-client/src/App.vue
+++ 盛诗博的Web前端学习系统大全/Demo21_硅谷外卖/code/gshop-client/src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    
+  <div id="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
 </template>
 
 <script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+
+import {reqFoodTypes} from './api'
+
 export default {
-  
+  components: {
+    FooterGuide
+  },
+  async mounted () {
+    const result = await reqFoodTypes()
+    console.log(result)
+  }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   .app
-    color: red
+    width 100%
+    height 100%
+    background #f5f5f5
 </style>
